a905ac14cb621cfeed524338baa2350238c08bba
Anastasia <kataurovas@icloud.com>
2021-06-24 20:57:50 +0300

    functions of translation from 10 number system and vice versa

--- crypt1.0/crypt1.0.py
+++ crypt1.0/crypt1.0.py
@@ -1,1 +1,23 @@
 
+def to_10(inp, osn):
+    p = 0
+    digit = 0
+    for i in range(len(inp)-1, -1, -1):
+        tmp = ord(inp[i])
+        if tmp >= 65:
+            digit += (tmp-55) * osn**p
+        else:
+            digit += (tmp-48) * osn**p
+        p += 1
+    return digit
+
+def from_10(inp, osn):
+    digit = ""
+    while inp > 0:
+        tmp = inp % osn
+        if tmp > 9:
+            digit += str(chr(tmp+55))
+        else:
+            digit += str(tmp)
+        inp //= osn
+    return digit[::-1]
