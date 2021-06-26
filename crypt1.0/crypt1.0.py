cc9422458a165971d7ab19dcb427046f68f68cf6
Anastasia <kataurovas@icloud.com>
2021-06-26 15:29:53 +0300

    new func

--- crypt1.0/crypt1.0.py
+++ crypt1.0/crypt1.0.py
@@ -1,13 +1,65 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
+import easygui
+from PIL import Image, ImageDraw 
 
 def start(flag):
 
     def encrypt():
         flag = 1
+        array_color = []
+        def create_color(crypt):
+            tmp = str(crypt)
+            l = len(tmp)
+            colors = []
+            while l >= 6:
+                t = tmp[l-6:]
+                tmp = tmp[:l-6]
+                colors.append(t)
+                l -= 6
+            if l == 0:
+                return colors
+            t = tmp
+            while l < 6:
+                t = "0" + t
+                l += 1
+            colors.append(t)
+            return colors
 
-        #create print_color
+        def print_color(colors):
+            if array_color:
+                for i in array_color:
+                    i.place_forget()
+                array_color.clear()
+            white = (255, 255, 255)
+            image1 = Image.new("RGB", (530, 530), white)
+            draw = ImageDraw.Draw(image1)
+            coord_x = 10 
+            coord_y = 150
+            x1, y1, x2, y2 = 0, 0, 50, 50
+            count = 0
+            ind = 0
+            for i in range(len(colors) - 1, -1, -1):
+                mycolor = '#'+ colors[i]
+                temp = Label(bg = mycolor, width = 5, height = 3)
+                temp.place(x = coord_x, y = coord_y)
+                ind += 1
+                widgets_crypte.append(temp)
+                array_color.append(temp)
+                draw.rectangle(((x1, y1), (x2, y2)), fill = mycolor)
+                x1 += 60; x2 += 60
+                coord_x += 50
+                #coord_y += 50
+                if (len(colors) - i) % 9 == 0:
+                    count += 1
+                    x1, y1, x2, y2 = 0, 60 * count, 50, 50 + (60 * count)
+                if ind % 10 == 0:
+                    coord_x = 10
+                    coord_y += 70
+            filename = "my_drawing.png"
+            image1.save(filename)
+
 
         def enter_message():
             s = message.get()
@@ -26,41 +78,41 @@ def start(flag):
 
         widgets_crypte = []
 
-        label_enter = Label(text = "Введите сообщение:", justify=CENTER, font="Verdana 14")
-        label_enter.place(x = 100, y = 200)
+        label_enter = Label(text = "Введите сообщение:", justify=CENTER, font="Verdana 14", bg = 'white')
+        label_enter.place(x = 45, y = 40)
         widgets_crypte.append(label_enter)
-
+    
         message = StringVar()
-        e = Entry(textvariable = message)
-        e.place(x = 100, y = 50)
+        e = Entry(textvariable = message, width = 60)
+        e.place(x = 45, y = 100)
         widgets_crypte.append(e)
 
-        button_enter = Button(text = "Ввод", command = enter_message)
-        button_enter.place(x = 150, y = 50)
+        button_enter = Button(text = "Ввод", command = enter_message, font="Verdana 10")
+        button_enter.place(x = 450, y = 98)
         widgets_crypte.append(button_enter)
 
         button_exit = Button(text = "Вернуться назад", command = exit)
-        button_exit.place(x = 100, y = 300)
+        button_exit.place(x = 380, y = 460)
         widgets_crypte.append(button_exit)
 
-
-    root = Tk()
-    root.resizable(width=False, height=False)
-    field = Canvas(root, width=500, height=500, bg='white')
-    field.pack()
+    if flag == 0:
+        root = Tk()
+        root.title("Шифровальщик")
+        field = Canvas(root, width=500, height=500, bg='white')
+        field.pack()
 
     widgets_main = []
 
-    greeting = Label(text = "����� ���������� � ������������.\n�� ������:", justify=CENTER, font="Verdana 14")
-    greeting.place(x = 100, y = 200)
+    greeting = Label(text = "Добро пожаловать в шифровальщик.\nВы хотите:", justify=CENTER, font="Verdana 14", bg = 'white')
+    greeting.place(x = 60, y = 50)
     widgets_main.append(greeting)
 
-    button_encrypt = Button(command = encrypt, text = "�����������", width = 20, height = 10)
-    button_encrypt.place(x = 200, y = 300)
+    button_encrypt = Button(command = encrypt, text = "Зашифровать", width = 20, height = 10)
+    button_encrypt.place(x = 135, y = 200)
     widgets_main.append(button_encrypt)
 
-    button_decrypt = Button(command = decrypt, text = "������������", width = 20, height = 10)
-    button_decrypt.place(x = 200, y = 400)
+    button_decrypt = Button(command = decrypt, text = "Расшифровать", width = 20, height = 10)
+    button_decrypt.place(x = 135, y = 300)
     widgets_main.append(button_decrypt)
 
     root.mainloop()
